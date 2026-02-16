03bcf66c10e25f5514697cb3d7138331f72fa995
Surajmatere <162417563+Surajmatere@users.noreply.github.com>
2026-02-16 16:10:26 +0530

    Update docker build command to use shell script

--- vars/docker_build.groovy
+++ vars/docker_build.groovy
@@ -1,4 +1,8 @@
 def call(String imageName, String tag, String username) {
-    // Use the parameters passed in the call
-    docker.build("${username}/${imageName}:${tag}")
+
+    sh """
+        docker build -t ${username}/${imageName}:${tag} .
+    """
+
 }
+
